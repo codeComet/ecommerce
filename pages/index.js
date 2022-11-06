eb8992c3b8758519090cbd0a7c6f8cc6a8b9a9c8
biishal <bishal@infodigita.com>
2022-11-06 12:03:30 +0600

    initialized

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return <div className="text-3xl font-bold text-red-400">Home</div>;
+  return <div className="text-3xl font-bold text-blue-400">Home</div>;
 };
 
 export default Home;
